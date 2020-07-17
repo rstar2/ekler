0d4fd3798f2e579f74ab86bf7fcb7f7290599692
Rumen Neshev <rstar2@abv.bg>
2020-07-17 14:05:12 +0300

    asd

--- functions/index.js
+++ functions/index.js
@@ -26,7 +26,7 @@ const db = require('./db')(admin.firestore(), colleklers, collhistory);
  *
  * Add (e.g virtual owe) ekler(s) from someone to someone and store in Firestore DB.
  */
-exports.dbAddEkler_app = functions.https.onCall(async (data, context) => {
+exports.addEklers = functions.https.onCall(async (data, context) => {
   // To ensure the client gets useful error details, return errors from a callable by throwing
   // (or returning a Promise rejected with) an instance of functions.https.HttpsError.
   // The error has a code attribute that can be one of the values listed at functions.https.HttpsError.
@@ -50,7 +50,7 @@ exports.dbAddEkler_app = functions.https.onCall(async (data, context) => {
   //         'one arguments "text" containing the message text to add.');
   //   }
 
-  console.log('New Ekler(s) add :', data);
+  console.log('Add Eklers :', data);
 
   let errorInvalidArgument;
   if (!data.from) {
@@ -72,82 +72,44 @@ exports.dbAddEkler_app = functions.https.onCall(async (data, context) => {
   // add in the 'history' collection finally
   await db.addHistory(data);
 
-  console.log('New Ekler(s) added :', data);
+  console.log('Added Eklers :', data);
 
   return true;
 });
 
-// /**
-//  * This is HTTPS triggered function
-//  *
-//  * Store uploaded image meta data in the database.
-//  * This function's HTTP URL is registered as a notification WebHook that is called by Cloudinary
-//  * when upload is successful
-//  */
-// exports.dbAddPlaceWeb = functions.https.onRequest(async (req, res) => {
-//   // enable CORS
-//   // these are needed for the preflight response and the actual response
-//   res.setHeader('Access-Control-Allow-Origin', '*'); // TODO: do this for certain hosts only
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   //   res.setHeader('Access-Control-Expose-Headers', 'XXXX');
-//   const method = req.method.toUpperCase();
-//   if (method === 'OPTIONS') {
-//     // preflight
+/**
+ * This is HTTPS callable function
+ *
+ * Add (e.g virtual owe) ekler(s) from someone to someone and store in Firestore DB.
+ */
+exports.checkoutEklers = functions.https.onCall(async (data, context) => {
+  // Checking that the user is authenticated.
+  if (!context.auth) {
+    // Throwing an HttpsError so that the client gets the error details.
+    throw new functions.https.HttpsError('unauthenticated', 'The function must be called ' + 'while authenticated.');
+  }
 
-//     // reflect the request headers
-//     res.setHeader('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-//     // allow all methods 'GET,HEAD,PUT,PATCH,POST,DELETE' or just what is expected
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-//     // res.setHeader('Access-Control-Max-Age', 'XXXXX');
+  console.log('Checkout Eklers :', data);
 
-//     // Safari (and potentially other browsers) need content-length 0, for 204 or they just hang waiting for a body
-//     res.setHeader('Content-Length', '0');
+  let errorInvalidArgument;
+  if (!data.from) {
+    errorInvalidArgument = 'No "uid" present - cannot checkout Eklers';
+  } else if (!data.to) {
+    errorInvalidArgument = 'No "to" present - cannot checkout Eklers';
+  }
+  if (errorInvalidArgument) {
+    console.log(errorInvalidArgument);
+    // terminate the function and send response
+    throw new functions.https.HttpsError('invalid-argument', errorInvalidArgument);
+  }
 
-//     res.statusCode = 204;
-//     res.end();
-//     return;
-//   }
-//   // actual response
+  // add in the 'eklers' collection
+  //   await db.addEkler(data);
 
-//   //   console.log(
-//   //     'Request headers:',
-//   //     Object.keys(req.headers)
-//   //       .map(header => header + '=' + req.headers[header])
-//   //       .join(',')
-//   //   );
-//   //   console.log('Request body:', req.body);
+  // add in the 'history' collection finally
+  await db.addHistory(data);
 
-//   // The body of the request is automatically parsed based on the 'content-type' header
-//   // and made available via your HTTP function's arguments.
-//   // If it's 'application/json' and sent data is '{"name":"John"}' so request.body will be valid JSON
+  console.log('Checked-out Eklers :', data);
 
-//   const { body } = req;
-
-//   const { uid, location, title, description, tags, url, meta = {} } = body;
-
-//   if (!uid) {
-//     const message = "No uid present - don't add it in the DB";
-//     console.log(message);
-//     // terminate the function and send response
-//     return res.status(500).send(message);
-//   }
-
-//   const { latitude, longitude } = location;
-
-//   if (!latitude || !longitude) {
-//     const message = "No GPS location present in the image metadata - don't add it in the DB";
-//     console.log(message);
-//     // terminate the function and send response
-//     return res.status(500).send(message);
-//   }
-
-//   // use 'creation date' from the upload response
-//   const createdAt = Date.now();
-
-//   const place = await db.addPlace({ uid, title, description, location, tags, url, createdAt, meta });
-
-//   console.log(`New place added to DB  - ${place.id}, location:`, place.location);
-
-//   // send response
-//   res.status(200).json(place);
-// });
+  return true;
+});
