22243199d916a50f8253d0fbf2c4ebdea2113308
Rumen Neshev <rstar2@abv.bg>
2020-07-31 23:26:16 +0300

    T2

--- src/firebase-configurePushNotifications.js
+++ src/firebase-configurePushNotifications.js
@@ -25,49 +25,56 @@ export default swReg => {
   messaging
     .requestPermission()
     .then(() => messaging.getToken())
-    .then(token => console.log('OK', token))
-    .catch(error => console.error(error));
+    .then(token => console.log('FCM token OK:', token))
+    .catch(error => {
+      console.error('FCM token failed:', error);
+      return messaging.deleteToken();
+    });
 
-//   // Get Instance ID token. Initially this makes a network call, once retrieved
-//   // subsequent calls to getToken will return from cache.
-//   messaging
-//     .getToken()
-//     .then(currentToken => {
-//       if (currentToken) {
-//         sendTokenToServer(currentToken);
-//         updateUIForPushEnabled(currentToken);
-//       } else {
-//         // Show permission request.
-//         console.log('No Instance ID token available. Request permission to generate one.');
-//         // Show permission UI.
-//         updateUIForPushPermissionRequired();
-//         setTokenSentToServer(false);
-//       }
-//     })
-//     .catch(err => {
-//       console.log('An error occurred while retrieving token. ', err);
-//       showToken('Error retrieving Instance ID token. ', err);
-//       setTokenSentToServer(false);
-//     });
+  messaging.onTokenRefresh(() => {
+    console.log('FCM token refreshed');
+  });
 
-//   // Callback fired if Instance ID token is updated.
-//   messaging.onTokenRefresh(() => {
-//     messaging
-//       .getToken()
-//       .then(refreshedToken => {
-//         console.log('Token refreshed.');
-//         // Indicate that the new Instance ID token has not yet been sent to the
-//         // app server.
-//         setTokenSentToServer(false);
-//         // Send Instance ID token to app server.
-//         sendTokenToServer(refreshedToken);
-//         // ...
-//       })
-//       .catch(err => {
-//         console.log('Unable to retrieve refreshed token ', err);
-//         showToken('Unable to retrieve refreshed token ', err);
-//       });
-//   });
+  //   // Get Instance ID token. Initially this makes a network call, once retrieved
+  //   // subsequent calls to getToken will return from cache.
+  //   messaging
+  //     .getToken()
+  //     .then(currentToken => {
+  //       if (currentToken) {
+  //         sendTokenToServer(currentToken);
+  //         updateUIForPushEnabled(currentToken);
+  //       } else {
+  //         // Show permission request.
+  //         console.log('No Instance ID token available. Request permission to generate one.');
+  //         // Show permission UI.
+  //         updateUIForPushPermissionRequired();
+  //         setTokenSentToServer(false);
+  //       }
+  //     })
+  //     .catch(err => {
+  //       console.log('An error occurred while retrieving token. ', err);
+  //       showToken('Error retrieving Instance ID token. ', err);
+  //       setTokenSentToServer(false);
+  //     });
+
+  //   // Callback fired if Instance ID token is updated.
+  //   messaging.onTokenRefresh(() => {
+  //     messaging
+  //       .getToken()
+  //       .then(refreshedToken => {
+  //         console.log('Token refreshed.');
+  //         // Indicate that the new Instance ID token has not yet been sent to the
+  //         // app server.
+  //         setTokenSentToServer(false);
+  //         // Send Instance ID token to app server.
+  //         sendTokenToServer(refreshedToken);
+  //         // ...
+  //       })
+  //       .catch(err => {
+  //         console.log('Unable to retrieve refreshed token ', err);
+  //         showToken('Unable to retrieve refreshed token ', err);
+  //       });
+  //   });
 
   // Handle incoming messages. Called when:
   // - a message is received while the app has focus
